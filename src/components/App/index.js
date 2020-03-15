61c322e280326af14164c7d26d0fcab8c1ad10e5
Dimitri Marion <contact@dimitrimarion.com>
2020-03-15 16:14:59 +0100

    add location input

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,32 +3,62 @@ import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Input from "@material-ui/core/Input";
 
-const PATH_BASE = "https://jobs.github.com/positions.json?";
+const PATH_BASE = "https://jobs.github.com/positions.json";
 const PARAM_DESCRIPTION = "description=";
 const PARAM_LOCATION = "location=";
 const PARAM_PAGE = "page=";
 
 const App = () => {
-  const [lastPositions, setLastPositions] = useState([]);
+  const [positions, setPositions] = useState([]);
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
 
   useEffect(() => {
+    // Fetch latest jobs posted by default
+    fetchPositions("");
+  }, []);
+
+  const handleChange = event => setDescription(event.target.value);
+
+  const handleChangeLocation = event => setLocation(event.target.value);
+
+  const handleSubmit = event => {
+    fetchPositions(description, location);
+    event.preventDefault();
+  };
+
+  const fetchPositions = (description, location = "", page = 0) => {
     fetch(
-      `https://cors-anywhere.herokuapp.com/${PATH_BASE}${PARAM_DESCRIPTION}&${PARAM_PAGE}`
+      `https://cors-anywhere.herokuapp.com/${PATH_BASE}?${PARAM_DESCRIPTION}${description}&${PARAM_LOCATION}${location}&${PARAM_PAGE}${page}`
     )
       .then(response => response.json())
-      .then(result => setLastPositions(result));
-  }, []);
+      .then(result => setPositions(result))
+      .catch(error => error);
+  };
 
   return (
     <Fragment>
       <CssBaseline />
-      <form>
-        <Input placeholder="role"></Input>
-        <Button variant="contained">Default</Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="role"
+          value={description}
+          onChange={handleChange}
+        ></Input>
+        <Input
+          placeholder="location"
+          value={location}
+          onChange={handleChangeLocation}
+        ></Input>
+        <Button variant="contained" type="submit">
+          Default
+        </Button>
       </form>
       <ul>
-        {lastPositions.map(({ company }) => (
-          <li key={company}>{company}</li>
+        {positions.map(({ id, company, title }) => (
+          <li key={id}>
+            {company} *** {title}
+          </li>
         ))}
       </ul>
     </Fragment>
