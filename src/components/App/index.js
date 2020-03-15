829a6f3126465df2c4f0b51426b3bfe16f9db537
Dimitri Marion <contact@dimitrimarion.com>
2020-03-15 13:57:26 +0100

    add fetch github jobs api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Input from "@material-ui/core/Input";
+
+const PATH_BASE = "https://jobs.github.com/positions.json?";
+const PARAM_DESCRIPTION = "description=";
+const PARAM_LOCATION = "location=";
+const PARAM_PAGE = "page=";
 
 const App = () => {
+  const [lastPositions, setLastPositions] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/${PATH_BASE}${PARAM_DESCRIPTION}&${PARAM_PAGE}`
+    )
+      .then(response => response.json())
+      .then(result => setLastPositions(result));
+  }, []);
+
   return (
     <Fragment>
       <CssBaseline />
-      <Button variant="contained">Default</Button>
+      <form>
+        <Input placeholder="role"></Input>
+        <Button variant="contained">Default</Button>
+      </form>
+      <ul>
+        {lastPositions.map(({ company }) => (
+          <li key={company}>{company}</li>
+        ))}
+      </ul>
     </Fragment>
   );
 };
