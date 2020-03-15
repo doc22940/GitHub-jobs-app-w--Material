540ce1d4d0b3177ed3af24c53b6c391845d67393
Dimitri Marion <contact@dimitrimarion.com>
2020-03-15 10:43:03 +0100

    added material-ui

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./index.css";
 import dog from "@images/dog.jpg";
+import Button from "@material-ui/core/Button";
 
 const App = () => {
   return (
     <main className="App">
       <h1>Hello World!</h1>
       <img src={dog} alt="" />
+      <Button variant="contained">Default</Button>
     </main>
   );
 };
