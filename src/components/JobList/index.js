435c77e854645450cec91432e2dff3feffc15bf7
Dimitri Marion <contact@dimitrimarion.com>
2020-03-16 22:33:46 +0100

    style job list

--- src/components/JobList/index.js
+++ src/components/JobList/index.js
@@ -2,6 +2,11 @@ import React, { Fragment } from "react";
 import ErrorSnackbar from "./ErrorSnackbar";
 import Box from "@material-ui/core/Box";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
 
 const JobList = ({ positions, isError, isLoading }) => {
   return (
@@ -18,13 +23,32 @@ const JobList = ({ positions, isError, isLoading }) => {
           <CircularProgress color="secondary" />
         </Box>
       ) : (
-        <ul>
-          {positions.map(({ id, company, title }) => (
-            <li key={id}>
-              {company} *** {title}
-            </li>
+        <List>
+          {positions.map(({ id, company, title, location, created_at }) => (
+            <Fragment key={id}>
+              <ListItem>
+                <Grid
+                  container
+                  alignItems="center"
+                  justify="space-between"
+                  spacing={2}
+                >
+                  <Grid item xs={8}>
+                    <ListItemText primary={title} secondary={company} />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <ListItemText
+                      primary={location}
+                      secondary={created_at}
+                      style={{ textAlign: "right" }}
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider component="li" />
+            </Fragment>
           ))}
-        </ul>
+        </List>
       )}
     </Fragment>
   );
