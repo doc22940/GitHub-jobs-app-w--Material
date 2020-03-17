671517b3b82098f9b8ee24f0c10310ba05831f76
Dimitri Marion <contact@dimitrimarion.com>
2020-03-17 17:59:44 +0100

    add how to apply to PageDescription

--- src/components/JobDescription/index.js
+++ src/components/JobDescription/index.js
@@ -10,9 +10,25 @@ import ErrorSnackbar from "../JobList/ErrorSnackbar";
 import { Typography } from "@material-ui/core";
 import ListItemText from "@material-ui/core/ListItemText";
 import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  summary: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(16),
+    fontWeight: theme.typography.fontWeightRegular
+  }
+}));
 
 const JobDescription = () => {
+  const classes = useStyles();
   const { id } = useParams();
   const [{ data: position, isLoading, isError }] = useGitHubApi(
     `${CORS_PROXY}https://jobs.github.com/positions/${id}.json`,
@@ -56,7 +72,19 @@ const JobDescription = () => {
               />
             </Grid>
           </Grid>
-          <Divider />
+          <ExpansionPanel elevation={3}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+              className={classes.summary}
+            >
+              <Typography className={classes.heading}>How to apply</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              {ReactHtmlParser(position.how_to_apply)}
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
           {ReactHtmlParser(position.description)}
         </Container>
       )}
